a97c6bea3de8372bce3c654566fc076839ce0c5d
devopsdeepdive <95200678+devopsdeepdive@users.noreply.github.com>
2025-07-10 11:42:19 +0530

    Update eks.tf

--- EKS-CLuster/eks.tf
+++ EKS-CLuster/eks.tf
@@ -33,7 +33,7 @@ resource "aws_eks_node_group" "worker_node_group" {
   }
 
   depends_on = [
-
+    aws_eks_cluster.eks_cluster,
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
